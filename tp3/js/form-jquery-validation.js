fef0b2d8b7c69646e4fe0e9ac457809030d35b99
zainabelgh <32839806+zainabelgh@users.noreply.github.com>
2017-11-15 18:33:35 +0100

    Add files via upload

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -9,7 +9,7 @@ $(document).ready(function () { //pour chargement de la page
                 $('#myModal').modal("show");
             }
             else {
-                $(".modal-body").html('<p>Vous êtes née le '+$('#date').val()+' et vous habitez </p>  </br> <img style="max-width:100%" src="https://maps.googleapis.com/maps/api/staticmap?center='+$('#address').val()+'&markers='+$('#address').val()+'&size=800x400&zoom=13"/> </br> <a href="http://maps.google.com/maps?q='+$('#address').val()+'">'+$('#address').val()+' </a>');
+                $(".modal-body").html('<p>Vous êtes née le '+$('#date').val()+', et vous habitez à : </p>  </br> <a href="http://maps.google.com/maps?q='+$('#address').val()+'"><img style="max-width:100%" src="https://maps.googleapis.com/maps/api/staticmap?center='+$('#address').val()+'&markers='+$('#address').val()+'&size=800x400&zoom=13"/> </br> '+$('#address').val()+' </a>');
                 $(".modal-title").text("Bienvenue "+$("#nom").val());
                 $('#myModal').modal("show");
     
